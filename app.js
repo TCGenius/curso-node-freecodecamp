b6ab0292a0b112f8d544a9260e48c62f6ea25d4c
Andres Alvarez <aalvarez@focasoftware.local>
2023-02-22 16:45:13 -0300

    Events

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-const EventEmitter = require('events');
+//Esta constante es solo para fines didácticos.
+const promesaCumplida = false;
 
+//Se crea una instancia del objeto Promise. resolve y reject ya están definidos.
+//Cuando resuelva, va a ganar el valor "¡Promesa cumplida!". En el reject, se pasa "promesa rechazada".
+const miPromesa = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (promesaCumplida) {
+      resolve('¡Promesa cumplida!');
+    }
+    reject('Promesa rechazada.');
+  }, 3000)
+});
+
+//Acá se toma como argumento el valor puesto dentro de resolve(). 
+miPromesa.then((valor) => {
+  console.log(valor);
+})
